bf885d432519c2ca263b912448ec229fca6cb721
Kalbir Sohi <kalbir@users.noreply.github.com>
2017-08-16 14:52:48 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    demo-8
                 </div>
 
                 <div class="links">
